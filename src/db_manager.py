be98d1175aefe15d981f7d97a76fc53f8070873e
iGhostCat <alfa-1002@mail.ru>
2025-09-10 22:52:10 +0300

    work progress

--- src/db_manager.py
+++ src/db_manager.py
@@ -15,6 +15,40 @@ class DB_Manager:
         conn.close()
         return result
 
+    def get_companies_and_vacancies_count(self):
+        """
+        Получает список всех компаний и количество вакансий у каждой компании.
+        Returns:
+            list: Список словарей с названием компании и количеством вакансий
+        """
+        query = """
+            SELECT e.name, COUNT(v.id) as vacancies_count
+            FROM employers e
+            LEFT JOIN vacancies v ON e.id = v.employer_id
+            GROUP BY e.id, e.name
+            ORDER BY vacancies_count DESC
+        """
+        params = config()
+        with psycopg2.connect(dbname=self.__db_name, **params) as conn:
+            try:
+                with conn.cursor() as cur:
+                    cur.execute(query)
+                    result = cur.fetchall()
+
+                    # Преобразуем результат в список словарей
+                    companies = []
+                    for row in result:
+                        companies.append({
+                            'company_name': row[0],
+                            'vacancies_count': row[1]
+                        })
+
+                    return companies
+
+            except Exception as e:
+                print(f"Ошибка при получении данных о компаниях: {e}")
+                return []
+
     def all_employers(self):
         query = "SELECT * FROM employers"
         return self.__execute_query(query)
--- src/main.py
+++ src/main.py
@@ -8,9 +8,25 @@ create_tables(db_name)
 insert_employers(db_name)
 insert_vacancies(db_name)
 
+print("Приветствую! Я ваш ассистент по работе с вакансиями\n"
+      "Введите цифру, соответствующую нужному вам действию:\n"
+      "1) ")
+
+
+
+
+
+
+
+
+
+
+'''
 db_manager = DB_Manager(db_name)
 print (db_manager.all_employers())
 print (db_manager.vacancies_from_selected_employers())
 print( db_manager.get_avg_salary())
 print (db_manager.get_vacancies_with_higher_salary())
 print (db_manager.get_vacancies_with_keyword('Сварщик'))
+print (db_manager.get_companies_and_vacancies_count())
+'''
